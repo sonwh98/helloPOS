f1007742f66db57ad148fa5b6c906f4b085db63f
sto <son.c.to@gmail.com>
2014-04-09 23:51:41 -0400

    initial commit

---
+++ src/androidTest/java/jp/leafytree/android/hello/HelloActivityJavaTest.java
@@ -0,0 +1,26 @@
+package jp.leafytree.android.hello;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+public class HelloActivityJavaTest extends ActivityInstrumentationTestCase2<HelloActivity> {
+    public Solo solo;
+
+    public HelloActivityJavaTest() {
+        super(HelloActivity.class);
+    }
+
+    @Override
+    public void setUp() {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void test1() {
+        assertTrue(true);
+    }
+
+    public void test2() {
+        assertTrue(solo.waitForText("Hello. I'm Java 2!"));
+    }
+}
---
+++ src/androidTest/scala/jp/leafytree/android/hello/HelloActivityTest.scala
@@ -0,0 +1,21 @@
+package jp.leafytree.android.hello
+
+import android.test.ActivityInstrumentationTestCase2
+import com.robotium.solo.Solo
+import junit.framework.Assert
+
+class HelloActivityTest extends ActivityInstrumentationTestCase2[HelloActivity](classOf[HelloActivity]) {
+  var solo: Solo = _
+
+  override def setUp() {
+    solo = new Solo(getInstrumentation(), getActivity())
+  }
+
+  def test1() {
+    Assert.assertTrue(true)
+  }
+
+  def test2() {
+    solo.waitForText("Hello. I'm Java !")
+  }
+}
