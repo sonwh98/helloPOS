d2ab55521ff375d286c064de73dc3d29ac2c225b
sto <son.c.to@gmail.com>
2014-05-08 23:43:19 -0400

    print as bitmap

--- src/main/scala/com/datayumyum/helloPOS/Printer.scala
+++ src/main/scala/com/datayumyum/helloPOS/Printer.scala
@@ -4,6 +4,9 @@ import com.starmicronics.stario.{StarIOPortException, StarIOPort, StarPrinterSta
 import java.text.SimpleDateFormat
 import java.util.Date
 import android.util.Log
+import android.text.{TextPaint, Layout, StaticLayout}
+import android.graphics._
+import com.StarMicronics.StarIOSDK.StarBitmap
 
 object Printer {
   val TAG = "com.datayumyum.pos.Printer"
@@ -41,21 +44,51 @@ object Printer {
   //  }
 
   def print(receipt: Receipt) {
-    val SET_WIDTH: Array[Byte] = Array(0x1d, 0x57, 0x40, 0x32)
-    val ALIGN_CENTER: Array[Byte] = Array(0x1b, 0x61, 0x01)
-    val ALIGN_LEFT: Array[Byte] = Array(0x1b, 0x61, 0x00)
-    val HORIZONTAL_TAB: Array[Byte] = Array(0x1b, 0x44, 0x02, 0x10, 0x22, 0x00)
-    val NEW_LINE: Array[Byte] = "\n\n\n\n\n".getBytes()
+    val paint: Paint = new Paint
+    paint.setStyle(Paint.Style.FILL)
+    paint.setColor(Color.BLACK)
+    paint.setAntiAlias(true)
+    var style: Int = 0
+    var font: Typeface = Typeface.DEFAULT
+    val typeface: Typeface = Typeface.create(font, style)
+    paint.setTypeface(typeface)
+    val textSize = 30
+    val paperWidth = 576
+    paint.setTextSize(textSize)
+    val textpaint: TextPaint = new TextPaint(paint)
 
-    val header: Array[Byte] = (receipt.store.toString() + "\n").getBytes()
-    val body: Array[Byte] = receipt.lineItems.map {
+    val header = receipt.store.toString()
+    val body = receipt.lineItems.map {
       case (quantity, item) => {
         f"$quantity%s ${item.name} ${item.price * quantity}"
       }
-    }.mkString("\n").getBytes()
-    val chinese = "小马词典".getBytes("UTF-8")
-    val cmd: Array[Byte] = SET_WIDTH ++ ALIGN_CENTER ++ header ++ ALIGN_LEFT ++ HORIZONTAL_TAB ++ body ++ chinese ++ NEW_LINE
-    sendCommand(cmd)
+    }.mkString("\n")
+    val chinese = "小马词典"
+    val headerLayout: StaticLayout = new StaticLayout(header, textpaint, paperWidth, Layout.Alignment.ALIGN_CENTER, 1, 0, false)
+    val headerBitmap: Bitmap = Bitmap.createBitmap(headerLayout.getWidth, headerLayout.getHeight, Bitmap.Config.RGB_565)
+    val c: Canvas = new Canvas(headerBitmap)
+    c.drawColor(Color.WHITE)
+    c.translate(0, 0)
+    c.drawLine(0.0f, headerLayout.getHeight, headerLayout.getWidth, headerLayout.getHeight, textpaint)
+    headerLayout.draw(c)
+
+    val starbitmap = new StarBitmap(headerBitmap, false, paperWidth)
+    val compression = true
+    val pageMode = true
+    val hearderCmd = starbitmap.getImageEscPosDataForPrinting(compression, pageMode)
+
+
+    val bodyLayout: StaticLayout = new StaticLayout(body, textpaint, paperWidth, Layout.Alignment.ALIGN_NORMAL, 1, 0, false)
+    val bodyBitMap = Bitmap.createBitmap(bodyLayout.getWidth, bodyLayout.getHeight, Bitmap.Config.RGB_565)
+    val c1 = new Canvas(bodyBitMap)
+    c1.drawColor(Color.WHITE)
+    c1.translate(0, 0)
+    bodyLayout.draw(c1)
+    val starBodyBitmap = new StarBitmap(bodyBitMap, false, paperWidth)
+    val bodyCmd = starBodyBitmap.getImageEscPosDataForPrinting(compression, pageMode)
+    val NEW_LINES: Array[Byte] = "\n\n\n\n".getBytes()
+
+    sendCommand(hearderCmd ++ bodyCmd ++ NEW_LINES)
   }
 
   def sendCommand(cmd: Array[Byte]) {
