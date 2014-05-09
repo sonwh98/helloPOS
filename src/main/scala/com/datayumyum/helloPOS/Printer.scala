19f42a5728d66338a8135469e071220e02b3d72b
sto <son.c.to@gmail.com>
2014-05-09 00:09:52 -0400

    refactor: extract createHeaderCmd and createBodyCmd

--- src/main/scala/com/datayumyum/helloPOS/Printer.scala
+++ src/main/scala/com/datayumyum/helloPOS/Printer.scala
@@ -23,39 +23,43 @@ object Printer {
     val paperWidth = 576
     paint.setTextSize(textSize)
     val textpaint: TextPaint = new TextPaint(paint)
-
-    val header = receipt.store.toString()
-    val body = receipt.lineItems.map {
-      case (quantity, item) => {
-        f"$quantity%s ${item.name} ${item.price * quantity}"
-      }
-    }.mkString("\n")
-    val chinese = "小马词典"
-    val headerLayout: StaticLayout = new StaticLayout(header, textpaint, paperWidth, Layout.Alignment.ALIGN_CENTER, 1, 0, false)
-    val headerBitmap: Bitmap = Bitmap.createBitmap(headerLayout.getWidth, headerLayout.getHeight, Bitmap.Config.RGB_565)
-    val c: Canvas = new Canvas(headerBitmap)
-    c.drawColor(Color.WHITE)
-    c.translate(0, 0)
-    c.drawLine(0.0f, headerLayout.getHeight, headerLayout.getWidth, headerLayout.getHeight, textpaint)
-    headerLayout.draw(c)
-
-    val starbitmap = new StarBitmap(headerBitmap, false, paperWidth)
     val compression = true
     val pageMode = true
-    val hearderCmd = starbitmap.getImageEscPosDataForPrinting(compression, pageMode)
 
+    def createHeaderCmd(): Array[Byte] = {
+      val header = receipt.store.toString()
+      val headerLayout: StaticLayout = new StaticLayout(header, textpaint, paperWidth, Layout.Alignment.ALIGN_CENTER, 1, 0, false)
+      val headerBitmap: Bitmap = Bitmap.createBitmap(headerLayout.getWidth, headerLayout.getHeight, Bitmap.Config.RGB_565)
+      val c: Canvas = new Canvas(headerBitmap)
+      c.drawColor(Color.WHITE)
+      c.translate(0, 0)
+      c.drawLine(0.0f, headerLayout.getHeight, headerLayout.getWidth, headerLayout.getHeight, textpaint)
+      headerLayout.draw(c)
+      val starbitmap = new StarBitmap(headerBitmap, false, paperWidth)
+      starbitmap.getImageEscPosDataForPrinting(compression, pageMode)
+    }
 
-    val bodyLayout: StaticLayout = new StaticLayout(body, textpaint, paperWidth, Layout.Alignment.ALIGN_NORMAL, 1, 0, false)
-    val bodyBitMap = Bitmap.createBitmap(bodyLayout.getWidth, bodyLayout.getHeight, Bitmap.Config.RGB_565)
-    val c1 = new Canvas(bodyBitMap)
-    c1.drawColor(Color.WHITE)
-    c1.translate(0, 0)
-    bodyLayout.draw(c1)
-    val starBodyBitmap = new StarBitmap(bodyBitMap, false, paperWidth)
-    val bodyCmd = starBodyBitmap.getImageEscPosDataForPrinting(compression, pageMode)
+    def createBodyCmd(): Array[Byte] = {
+      val body = receipt.lineItems.map {
+        case (quantity, item) => {
+          f"$quantity%s ${item.name} ${item.price * quantity}"
+        }
+      }.mkString("\n")
+
+      val bodyLayout: StaticLayout = new StaticLayout(body, textpaint, paperWidth, Layout.Alignment.ALIGN_NORMAL, 1, 0, false)
+      val bodyBitMap = Bitmap.createBitmap(bodyLayout.getWidth, bodyLayout.getHeight, Bitmap.Config.RGB_565)
+      val c1 = new Canvas(bodyBitMap)
+      c1.drawColor(Color.WHITE)
+      c1.translate(0, 0)
+      bodyLayout.draw(c1)
+      val starBodyBitmap = new StarBitmap(bodyBitMap, false, paperWidth)
+      starBodyBitmap.getImageEscPosDataForPrinting(compression, pageMode)
+    }
+
+    val headerCmd = createHeaderCmd()
+    val bodyCmd = createBodyCmd()
     val NEW_LINES: Array[Byte] = "\n\n\n\n".getBytes()
-
-    sendCommand(hearderCmd ++ bodyCmd ++ NEW_LINES)
+    sendCommand(headerCmd ++ bodyCmd ++ NEW_LINES)
   }
 
   def sendCommand(cmd: Array[Byte]) {
