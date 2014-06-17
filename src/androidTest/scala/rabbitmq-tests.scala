e1e1c2372b8a83af88388759b51c4b478ad17e92
sto <son.c.to@gmail.com>
2014-06-17 15:07:24 -0400

    rabbitmq

---
+++ src/androidTest/scala/rabbitmq-tests.scala
@@ -0,0 +1,21 @@
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.Connection
+import com.rabbitmq.client.Channel
+import com.rabbitmq.client.MessageProperties
+
+val factory = new ConnectionFactory()
+factory.setHost("localhost")
+factory.setPort(1234)
+val connection: Connection = factory.newConnection()
+val channel: Channel = connection.createChannel()
+
+val queue2 = "menu"
+val durable = true
+val exclusive = false
+channel.queueDeclare(queue2, durable, exclusive, false, null)
+val message = "一儿三四五六七八九十啊吧色:::0:::20:::365:::40"
+
+channel.basicPublish("", queue2, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes())
+channel.close()
+connection.close()
+println("foo")
