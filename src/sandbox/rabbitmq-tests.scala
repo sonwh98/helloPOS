46e098761e46c1a84a1404ff556f27c9568a81b6
sto <son.c.to@gmail.com>
2014-06-18 11:39:03 -0400

    rabbitmq

--- src/sandbox/rabbitmq-tests.scala
+++ src/sandbox/rabbitmq-tests.scala
@@ -4,19 +4,21 @@ import com.rabbitmq.client.Channel
 import com.rabbitmq.client.MessageProperties
 
 val factory = new ConnectionFactory()
-//factory.setHost("localhost")
-//factory.setPort(1234)
+factory.setHost("hive.kaicode.com")
+factory.setPort(5672)
+factory.setUsername("order")
+factory.setPassword("EisKisP1")
 val connection: Connection = factory.newConnection()
 val channel: Channel = connection.createChannel()
 
-val queue2 = "menu3"
+val queueName = "order-queue"
 val durable = true
 val exclusive = false
 val autoDelete = true
-channel.queueDeclare(queue2,  durable, exclusive, autoDelete, null)
+channel.queueDeclare(queueName, durable, exclusive, autoDelete, null)
 val message = "一儿三四五六七八九十啊吧色:::0:::20:::365:::40"
 
-channel.basicPublish("", queue2, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes())
+channel.basicPublish("", queueName, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes())
 channel.close()
 connection.close()
 println("foo")
