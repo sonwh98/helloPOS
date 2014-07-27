46fb24476d9e948eb5c179c629f7a94c41d06336
sto <son.c.to@gmail.com>
2014-07-27 14:42:29 -0400

    start scala repl

--- srepl.sh
+++ srepl.sh
@@ -1,1 +1,2 @@
-scala -cp ./build/intermediates/classes/debug:./lib/json.jar
+#scala -cp ./build/intermediates/classes/debug:./lib/json.jar
+scala -cp ./build/android-scala/variant/debug/scalaCompile:./lib/json.jar
