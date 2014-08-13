ab93732f92dfc3e5b7343da2e1d7f3e3f340caad
sto <son.c.to@gmail.com>
2014-08-13 06:06:26 +0100

    LoginActivity starts MainAcitivty

---
+++ src/main/scala/com/datayumyum/helloPOS/LoginActivity.scala
@@ -0,0 +1,26 @@
+package com.datayumyum.helloPOS
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import EventHandlers._
+
+class LoginActivity extends Activity {
+  val TAG = "com.datayumyum.helloPOS.LoginActivity"
+
+  lazy val loginButton: Button = findViewById(R.id.loginSubmitButton).asInstanceOf[Button]
+
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.login_activity)
+    loginButton.onClick { () =>
+      Log.i(TAG, s"${loginButton} pressed")
+      val intent = new Intent(this, classOf[MainActivity]);
+      startActivity(intent)
+    }
+
+  }
+}
