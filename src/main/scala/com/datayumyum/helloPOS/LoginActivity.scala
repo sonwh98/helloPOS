d547641e55682a9fd74be69f4474b89eed06c588
sto <son.c.to@gmail.com>
2014-08-22 21:59:51 +0100

    authentication

--- src/main/scala/com/datayumyum/helloPOS/LoginActivity.scala
+++ src/main/scala/com/datayumyum/helloPOS/LoginActivity.scala
@@ -7,7 +7,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.{Toast, Button}
+import android.widget.{EditText, Toast, Button}
 import com.datayumyum.helloPOS.EventHandlers._
 
 import scala.io.Source
@@ -17,6 +17,9 @@ class LoginActivity extends Activity {
   val TAG = "com.datayumyum.helloPOS.LoginActivity"
 
   lazy val loginButton: Button = findViewById(R.id.loginSubmitButton).asInstanceOf[Button]
+  lazy val emailEditText: EditText = findViewById(R.id.email).asInstanceOf[EditText]
+  lazy val passwordEditText: EditText = findViewById(R.id.password).asInstanceOf[EditText]
+
   implicit val exec = ExecutionContext.fromExecutor(
     new ThreadPoolExecutor(100, 100, 1000, TimeUnit.SECONDS,
       new LinkedBlockingQueue[Runnable]))
@@ -25,7 +28,9 @@ class LoginActivity extends Activity {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.login_activity)
     loginButton.onClick {
-      val url: String = s"http://hive.kaicode.com:3000/pos/authenticate?email=son.c.to@gmail.com&password=foobar"
+      val email = emailEditText.getText()
+      val password = passwordEditText.getText()
+      val url: String = s"http://hive.kaicode.com:3000/pos/authenticate?email=$email&password=$password"
       val f: Future[String] = Future {
         val result: String = Source.fromInputStream(new URL(url).openStream).mkString
         result
@@ -33,8 +38,13 @@ class LoginActivity extends Activity {
 
       f.onSuccess {
         case result =>
-          val intent = new Intent(this, classOf[PosActivity]);
-          startActivity(intent)
+          if (result == "true") {
+            Log.e(TAG, result.toString())
+            val intent = new Intent(this, classOf[PosActivity]);
+            startActivity(intent)
+          } else {
+            Log.w(TAG, s"cannot login ${url}")
+          }
       }
 
       f.onFailure {
