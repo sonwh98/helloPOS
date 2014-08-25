dd9640776c2495d2cdcec1b57957d2994190efa1
sto <son.c.to@gmail.com>
2014-08-25 10:02:01 -0400

    add android.jar to classpath

--- srepl.sh
+++ srepl.sh
@@ -1,2 +1,2 @@
 #scala -cp ./build/intermediates/classes/debug:./lib/json.jar
-scala -cp ./build/android-scala/variant/debug/scalaCompile:./lib/json.jar
+scala -cp ./build/android-scala/variant/debug/scalaCompile:/opt/android/platforms/android-19/android.jar
