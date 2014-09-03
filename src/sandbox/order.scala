7900d0ba67b9657a7e314993d2022dae6433714c
sto <son.c.to@gmail.com>
2014-09-03 14:34:47 -0400

    classpath for repl

---
+++ src/sandbox/order.scala
@@ -0,0 +1,3 @@
+import com.datayumyum.helloPOS._
+
+val store = Store.findById("17592186045418")
--- srepl.sh
+++ srepl.sh
@@ -1,2 +1,2 @@
 #scala -cp ./build/intermediates/classes/debug:./lib/json.jar
-scala -cp ./build/android-scala/variant/debug/scalaCompile:./lib/json.jar:/opt/android/platforms/android-19/android.jar
+scala -cp ./build/android-scala/variant/debug/scalaCompile:./build/intermediates/classes-proguard/debug/classes.jar:./lib/json.jar:/opt/android/platforms/android-19/android.jar
