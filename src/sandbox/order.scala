9f9e6f45838c8ebf4d4e938387111b6091f2f5b9
sto <son.c.to@gmail.com>
2014-09-04 10:09:20 -0400

    send order as an edn

--- src/sandbox/order.scala
+++ src/sandbox/order.scala
@@ -1,3 +1,13 @@
 import com.datayumyum.helloPOS._
 
 val store = Store.findById("17592186045418")
+
+
+val sandwiches = store.catalog("Sandwiches")
+val s1 = sandwiches(0)
+val i0 = s1.ingredients(0)
+val i1 = s1.ingredients(1)
+val i2 = s1.ingredients(2)
+val lineItem1 = LineItem(1, s1, List(i0, i1, i2))
+val order = Order(store, List(lineItem1))
+OrderMessenger.sendOrder(order.toEdn())
